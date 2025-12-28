41fd0da7740efd681fa4258d50ffd31f8dedc64a
yadavayush09 <ayushyadav11300@gmail.com>
2025-12-28 16:08:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ final_value = scaler.transform([all_value])
 
 model = RandomForestRegressor()
 model.fit(X,y)
-house_price = model.predict(final_value)
+house_price = model.predict(final_value)[0][0]
 
 with st.spinner('Predicting House Price'):
   time.sleep(3)
@@ -37,5 +37,6 @@ msg = f'''House Price is: $ (house_price*100000)'''
 st.success(msg)
 
 st.markdown('''**Design and Developed by: Ayush Yadav**''')
+
 
 
