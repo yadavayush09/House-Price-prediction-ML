6da3e5c3b8732e1b48a665f6c6772c8b33a7ea18
yadavayush09 <ayushyadav11300@gmail.com>
2025-12-28 16:29:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ scaled_X = scaler.fit_transform(X)
 final_value = scaler.transform([all_value])
 
 model = st.cache(RandomForestRegressor)()
-model.fit(X,y)
+st.cache(model.fit(X,y))
 house_price = model.predict(final_value)[0]
 
 with st.spinner('Predicting House Price'):
@@ -37,6 +37,7 @@ msg = f'''House Price is: $ {round(house_price*100000,2)}'''
 st.success(msg)
 
 st.markdown('''**Design and Developed by: Ayush Yadav**''')
+
 
 
 
